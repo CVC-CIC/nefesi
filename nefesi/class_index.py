- Added the visualization of labels captured on each N-topScoring on the interface - Solved bug with capture of receptive field on the concept selectivity calculation - Addded new operations for calculate the concept selectivity

nefesi/class_index.py
@@ -70,13 +70,14 @@ def get_concept_selectivity_idx(neuron_data, layer_data, network_data,index_by_l
 
     return np.array(concepts)
 
-def concept_selectivity_of_image(activations_mask, segmented_image, type='mean'):
+def concept_selectivity_of_image(activations_mask, segmented_image, type='mean'): #activations_mask
     """
     :param type: 'max' = only the max of every region, 'sum' sum of the pixels of a region, 'mean' the mean of the activation
-    in each region.
+    in each region, 'percent' the plain percent, 'activation' the max activation of the n-topScoring.
     :return:
     """
-    activations_mask = activations_mask.reshape(-1)
+    if not (type == 'activation' or type == 'percent'):
+        activations_mask = activations_mask.reshape(-1)
     ids, correspondency = np.unique(segmented_image, return_inverse=True)
     histogram = np.zeros(shape=len(ids), dtype=np.float)
     if type == 'max':
@@ -88,10 +89,16 @@ def concept_selectivity_of_image(activations_mask, segmented_image, type='mean')
     elif type == 'mean':
         for i in range(len(ids)):
             histogram[i] = np.mean(activations_mask[correspondency == i])
+    elif type == 'percent':
+        for i in range(len(ids)):
+            histogram[i] = np.sum(correspondency==i) / segmented_image.size
+    elif type == 'activation':
+        for i in range(len(ids)):
+            histogram[i] = activations_mask
     else:
         raise ValueError('Valid types: max, sum and mean. Type '+str(type)+' is not valid')
-    normalized_hist = histogram/np.sum(histogram)
-    return ids, normalized_hist
+    #normalized_hist = histogram/np.sum(histogram)
+    return ids, histogram
 
 
 def get_concept_selectivity_of_neuron(network_data, layer_name, neuron_idx, type='mean', concept='object'):
@@ -110,7 +117,8 @@ def get_concept_selectivity_of_neuron(network_data, layer_name, neuron_idx, type
 
     image_names = neuron.images_id
     images_ids = neuron.images_id
-    activations_masks = read_act.get_image_activation(network_data, image_names, layer_name, neuron_idx, type=1)
+    if not type == 'activation':
+        activations_masks = read_act.get_image_activation(network_data, image_names, layer_name, neuron_idx, type=1)
     """
     Change it for the code to obtain the object and parts matrix
     """
@@ -125,14 +133,15 @@ def get_concept_selectivity_of_neuron(network_data, layer_name, neuron_idx, type
     for i in range(len(images_ids)):
         #Crop for only use the receptive field
         ri, rf, ci, cf = receptive_field[neuron.xy_locations[i, 0], neuron.xy_locations[i, 1]]
-        cropped_activation_masks = activations_masks[i][ri:rf, ci:cf]
+        ri, rf, ci, cf = abs(ri), abs(rf), abs(ci), abs(cf)
         cropped_segmentation = segmentation[i][concept][ri:rf, ci:cf]
-
+        activation = norm_activations[i] if type=='activation' else activations_masks[i][ri:rf, ci:cf]
         #Make individual hist
-        ids, personal_hist = concept_selectivity_of_image(activations_mask=cropped_activation_masks,
+        ids, personal_hist = concept_selectivity_of_image(activations_mask=activation,
                                                           segmented_image=cropped_segmentation,
                                                           type=type)
-        personal_hist *= norm_activations[i]
+        if not type == 'activation':
+            personal_hist *= norm_activations[i]
 
         for id, value in zip(ids, personal_hist):
             if id in general_hist:
@@ -151,7 +160,7 @@ def get_concept_selectivity_of_neuron(network_data, layer_name, neuron_idx, type
 def translate_concept_hist(hist, concept):
     # Charge without index (redundant with pos) and without header
     translation = np.genfromtxt(CONCEPT_TRANSLATION_BASE_DIR+concept+'.csv', delimiter=',', dtype=np.str)[1:,1]
-    translated_hist = [(translation[id],value) for id, value in hist]
+    translated_hist = [(translation[element['id']],element['value']) for element in hist]
     return np.array(translated_hist, dtype=[('id', np.object), ('value', np.float)])
 
 

nefesi/interface/popup_windows/receptive_field_popup_window.py
@@ -9,6 +9,7 @@ except ImportError:
 from ...util.general_functions import get_image_masked
 from ...util.segmentation.utils import maskrcnn_colorencode
 from ...util.segmentation.Broden_analize import Segment_images
+from ...class_index import concept_selectivity_of_image, translate_concept_hist
 from PIL import ImageTk,Image
 import numpy as np
 
@@ -16,7 +17,7 @@ import numpy as np
 
 class ReceptiveFieldPopupWindow(object):
     def __init__(self, master, image_cropped,x_len,y_len, image_name, layer_name,
-                 neuron_idx,interface,x0,x1,y0,y1):
+                 neuron_idx,interface,x0,x1,y0,y1, actual_idx):
         self.window=Toplevel(master)
         self.network_data = interface.network_data
         self.image_name = image_name
@@ -28,6 +29,12 @@ class ReceptiveFieldPopupWindow(object):
         self.y0 = y0
         self.y1 = y1
         self.interface = interface
+        self.actual_idx = actual_idx
+        self.layer_data = self.network_data.get_layer_by_name(self.layer_name)
+        self.receptive_field = self.layer_data.receptive_field_map
+        self.neuron = self.layer_data.neurons_data[self.neuron_idx]
+        self.labels_printed = False
+        self.color_list = None
         self.window.title('Receptive Field of '+str(x_len)+'X'+str(y_len)+' (Neuron '+str(neuron_idx)+' '+layer_name+').')
         self.complete_image_frame = Frame(master=self.window)
         Label(master=self.complete_image_frame, text="Original image").pack(side=TOP)
@@ -38,6 +45,7 @@ class ReceptiveFieldPopupWindow(object):
         self.receptive_camp_frame = Frame(master=self.window)
         self.set_receptive_camp_frame(master=self.receptive_camp_frame)
         self.set_threshold_frame(master=self.receptive_camp_frame)
+        self.set_false_labels_frame(master=self.receptive_camp_frame)
         self.complete_image_frame.pack(side=LEFT)
         self.receptive_camp_frame.pack(side=RIGHT)
         self.cropped_image_frame.pack(side=RIGHT)
@@ -80,8 +88,17 @@ class ReceptiveFieldPopupWindow(object):
         else:
             img = self.network_data.dataset._load_image(self.image_name)
             if value == 5:
-                segmentation = Segment_images([self.network_data.dataset.src_dataset+self.image_name])
-                img = maskrcnn_colorencode(np.asarray(img), segmentation[0]['object'], np.random.rand(1000, 3) * .7 + .3)
+                segmentation = Segment_images([self.network_data.dataset.src_dataset+self.image_name])[0]['object']
+                if self.color_list is None:
+                    self.color_list = np.random.rand(1000, 3) * .7 + .3
+                img = maskrcnn_colorencode(np.asarray(img), segmentation, self.color_list)
+                if not self.labels_printed:
+                    ri, rf, ci, cf = self.receptive_field[
+                        self.neuron.xy_locations[self.actual_idx, 0], self.neuron.xy_locations[self.actual_idx, 1]]
+                    ri, rf, ci, cf = abs(ri),abs(rf),abs(ci),abs(cf)
+                    segmentation = segmentation[ri:rf, ci:cf]
+                    self.set_labels_frame(master=self.receptive_camp_frame, segment=segmentation, color_list=self.color_list)
+                    self.labels_printed = True
 
         img = self.interface.draw_rectangle_on_image(np.array(img), self.x0, self.x1, self.y0, self.y1)
         img = Image.fromarray(img.astype('uint8'))
@@ -101,3 +118,23 @@ class ReceptiveFieldPopupWindow(object):
         self.thr = Scale(master, from_=0, to=1, orient=HORIZONTAL, digits=4, resolution=0.001)
         self.thr.set(0.005)
         self.thr.pack(side=TOP)
+
+    def set_labels_frame(self, master, segment, color_list):
+        ids, freqs = concept_selectivity_of_image(activations_mask=None, segmented_image=segment, type='percent')
+        arr = [(id, freq, tuple(color_list[id])) for id, freq in zip(ids, freqs)]
+        arr = np.array(arr, dtype=[('id', np.int), ('value', np.float), ('color', np.object)])
+        arr_transl = translate_concept_hist(arr, 'object')
+        arr_comp = np.zeros(len(arr), dtype=[('id', np.object), ('value', np.float), ('color', np.object)])
+        arr_comp['id'], arr_comp['value'], arr_comp['color'] = arr_transl['id'], arr_transl['value'], arr['color']
+        arr_comp = np.sort(arr_comp, order = 'value')[::-1]
+        for label, freq, color in arr_comp:
+            text = label + '('+str(np.round(freq*100,2))+'%)'
+            ttk.Label(self.labels_frame, text=text, background=rgb_to_code(color)).pack(side=TOP, anchor="w")
+
+    def set_false_labels_frame(self, master,):
+        self.labels_frame = LabelFrame(master, text="Segments (on rec. field)", padx=2, pady=2)
+        self.labels_frame.pack(side=TOP, fill=X, expand="yes")
+def rgb_to_code(rgb):
+    if type(rgb[0]) is not int:
+        rgb = (int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255))
+    return "#%02x%02x%02x" % rgb