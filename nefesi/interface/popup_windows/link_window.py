Relevance decreasing adapted for be calculated in real time. I just found a bug on decreasing for Object, Part and Color, which are normalizing by himselfs and not by original activations, it need to be fixed in a future.

nefesi/interface/popup_windows/link_window.py
@@ -42,7 +42,8 @@ class LinkWindow(object):
         #self.font = ImageFont.truetype(font='sans-serif.ttf', size=16)
         self.original_neuron = self.network_data.get_neuron_of_layer(layer=original_layer, neuron_idx=neuron_idx)
         self.actual_link = 0
-        self.relevance = self.original_neuron.relevance_idx[self.ablated_layer]
+        self.relevance = self.original_neuron.get_relevance_idx(network_data=network_data, layer_name=original_layer,
+                                                                neuron_idx=neuron_idx, layer_to_ablate=ablated_layer)
         self.relevance_order = np.argsort(self.relevance)[::-1]
         self.basic_frame = ttk.Frame(master=self.window)
         self.decomposition_frame = ttk.Frame(self.basic_frame)
@@ -53,14 +54,13 @@ class LinkWindow(object):
 
     def set_decomposition_frame(self, master):
         image_frame = ttk.Frame(master=master)
-        neuron_label, relevance_label = self.set_decomposition_label(master)
         neuron_of_link_idx = self.relevance_order[self.actual_link]
         neuron_of_link = self.network_data.get_neuron_of_layer(layer=self.ablated_layer, neuron_idx=neuron_of_link_idx)
         current_image = neuron_of_link._neuron_feature
         current_image = current_image.resize((int(self.image_actual_size[0]/2),int(self.image_actual_size[1]/2)), Image.ANTIALIAS)  # resize mantaining aspect ratio
         self.write_decreasing_on_image(img = current_image)
         img = ImageTk.PhotoImage(current_image)
-
+        neuron_label, relevance_label = self.set_decomposition_label(master)
         self.panel_image = ttk.Label(master=image_frame, image=img)
         self.panel_image.image = img
         self.panel_image.bind("<Double-Button-1>",lambda event: self._on_link_image_click(event,
@@ -112,16 +112,16 @@ class LinkWindow(object):
         panel.configure(image=img)
         panel.image = img
 
-    def write_decreasing_on_image(self, img):
+    def write_decreasing_on_image(self, img, print_decreasing_matrix=True):
         neuron_idx = self.relevance_order[self.actual_link]
-        a = self.original_neuron.get_relevance_idx(network_data=self.network_data, layer_name=self.original_layer,
+        _, concept, type = self.original_neuron.get_relevance_idx(network_data=self.network_data,
+                                                layer_name=self.original_layer,
                                                neuron_idx = self.neuron_idx, layer_to_ablate=self.ablated_layer,
-                                               for_neuron=neuron_idx, return_decreasing=True)
-        concept =  ('Not Ready', 0.0)#self.original_neuron.most_relevant_concept[self.ablated_layer][neuron_idx]
-        type = ('Not Ready', 0.0)#self.original_neuron.most_relevant_type[self.ablated_layer][neuron_idx]
+                                               for_neuron=neuron_idx, return_decreasing=True,
+                                                                  print_decreasing_matrix=print_decreasing_matrix)
         text_image = ImageDraw.Draw(img)
-        text_image.text((10, 0), "Decreased Type: "+type[0]+ ' -> '+str(type[1]), fill=0)
-        text_image.text((10, 15), "Decreased Concept: " + concept[0] + ' -> ' + str(concept[1]), fill=0)
+        text_image.text((10, 0), "Decreased Type: "+type['label']+ ' -> '+str(type['value']), fill=0)
+        text_image.text((10, 15), "Decreased Concept: " + concept['label'] + ' -> ' + str(concept['value']), fill=0)
 
     def _on_resize_image(self, event, panel,neuron_feature,top_widget):
         panel.update()

nefesi/network_data.py
@@ -19,6 +19,7 @@ from .class_index import get_concept_labels
 from .util.ColorNaming import colors as color_names
 from keras.layers import Conv2D, Input
 from keras.models import Sequential, Model, clone_model
+from keras import backend
 from tensorflow import Session
 import nefesi.util.GPUtil as gpu
 gpu.assignGPU()
@@ -485,7 +486,8 @@ class NetworkData(object):
         return relevance_idx
 
 
-    def get_relevance_by_ablation(self, layer_analysis, neuron, layer_to_ablate,path_model, for_neuron = None, return_decreasing = False):
+    def get_relevance_by_ablation(self, layer_analysis, neuron, layer_to_ablate,path_model, for_neuron = None,
+                                  return_decreasing = False, print_decreasing_matrix = False):
         """Returns the relevance of each neuron in the previous layer for neuron in layer_analysis
 
             :param self: Nefesi object
@@ -494,7 +496,10 @@ class NetworkData(object):
             :param neuron: Int with the neuron to analyze
             :return: A list with: the sum of the difference between the original max activations and the max activations after ablating each previous neuron
             """
+
         current_layer = self.get_layer_by_name(layer_analysis)
+        if return_decreasing:
+            pre_ablation_indexes = current_layer.get_all_index_of_a_neuron(network_data=self, neuron_idx=neuron)
         neuron_data = self.get_neuron_of_layer(layer_analysis, neuron)
         xy_locations = neuron_data.xy_locations
         image_names = neuron_data.images_id
@@ -508,7 +513,9 @@ class NetworkData(object):
             numb_ablated=layer_names.index(layer_to_ablate)
             numb_analysis=layer_names.index(layer_analysis)+1
             intermediate_layer_model = Model(inputs=cloned_model.input, outputs=cloned_model.get_layer(layer_to_ablate).output)
-            intermediate_output = intermediate_layer_model.predict(images)
+            # I don't know why it was generating a random error, because learning_phase() sometimes was an int, not function
+            backend.learning_phase = lambda: 0
+            intermediate_output = intermediate_layer_model.predict(x=images)
 
             DL_input = Input(cloned_model.layers[numb_ablated+1].input_shape[1:],name=layer_to_ablate)
 
@@ -534,7 +541,6 @@ class NetworkData(object):
             relevance_idx = []
             if return_decreasing:
                 max_concept_decreasing, max_type_decreasing = [], []
-            pre_ablation_indexes = current_layer.get_all_index_of_a_neuron(network_data=self, neuron_idx=neuron)
             range_of_neurons = range(intermediate_output.shape[-1]) if for_neuron is None else [for_neuron]
             for i in range_of_neurons:
                 intermediate_output2 = intermediate_output[..., i]*1 #To copy
@@ -550,9 +556,12 @@ class NetworkData(object):
                                                     norm_act=max_activations/original_activations[0],
                                                     activations_masks = ablated_neurons_predictions, thr_pc=0.0,
                                                     original_norm_act=original_norm_activations)
-
+                    if print_decreasing_matrix:
+                        print('Indexes decreasing for Neuron: '+str(neuron)+' - Layer: '+layer_analysis+'\n'
+                              '             On ablate Neuron: '+str(i)+' - Layer: '+layer_to_ablate+':\n')
                     max_concept, max_type = self.most_decreased_index(pre_indexes=pre_ablation_indexes,
-                                                           post_indexes=post_ablation_indexes)
+                                                           post_indexes=post_ablation_indexes,
+                                                                      print_indexes_decreasing=print_decreasing_matrix)
                     max_concept_decreasing.append(max_concept)
                     max_type_decreasing.append(max_type)
 
@@ -573,7 +582,7 @@ class NetworkData(object):
                 return relevance_idx[0]
 
 
-    def most_decreased_index(self, pre_indexes, post_indexes):
+    def most_decreased_index(self, pre_indexes, post_indexes, print_indexes_decreasing = False):
         indexes_decreased = self.indexes_decreasing(pre_indexes=pre_indexes, post_indexes=post_indexes)
         index_decreasing_by_key = np.array(list(map(lambda key:
                                                     (key, np.sum(indexes_decreased[key]['value'])), indexes_decreased)),
@@ -592,10 +601,22 @@ class NetworkData(object):
         if np.isclose(max_type['value'],0.0):
             max_type['label'] = 'None'
 
+        if print_indexes_decreasing:
+            print(self.indexes_decreasing_matrix_as_string(indexes_decreased))
 
         return (max_concept, max_type)
 
-
+    def indexes_decreasing_matrix_as_string(self, indexes_decreased):
+        string = ''
+        for concept, decreasing in indexes_decreased.items():
+            string += concept.capitalize()+':\t'
+            decreasing['value'] = np.round(decreasing['value'],decimals=3)
+            for label, value in decreasing:
+                if not np.isclose(value, 0):
+                    string+= ' '+label+' - '+str(value)+','
+            string = string[:-1]
+            string += '\n'
+        return string
     def indexes_decreasing(self, pre_indexes, post_indexes):
         indexes_decreased = {}
         for key in post_indexes.keys():

nefesi/neuron_data.py
@@ -300,36 +300,76 @@ class NeuronData(object):
                                                      layer_data, dataset)
         return self.selectivity_idx[key]
 
-    def get_relevance_idx(self,network_data, layer_name, neuron_idx, layer_to_ablate, for_neuron=None, return_decreasing=False):
-        if layer_to_ablate not in self.relevance_idx or np.sum(np.isclose(self.relevance_idx[layer_to_ablate], -1)) > 0:
+    def get_relevance_idx(self,network_data, layer_name, neuron_idx, layer_to_ablate, for_neuron=None,
+                          return_decreasing=False, print_decreasing_matrix=False):
+
+        need_to_calculate = print_decreasing_matrix or (layer_to_ablate not in self.relevance_idx ) or (return_decreasing and
+                            (layer_to_ablate not in self.most_relevant_concept or layer_to_ablate not in self.most_relevant_type))
+        if not need_to_calculate:
+            if for_neuron is None:
+                need_to_calculate = np.sum(np.isclose(self.relevance_idx[layer_to_ablate], -1)) > 0 or (return_decreasing
+                                and ((np.sum(self.most_relevant_concept[layer_to_ablate]['label'] == '') > 0) or
+                                     (np.sum(self.most_relevant_type[layer_to_ablate]['label'] == '') > 0)))
+            else:
+                need_to_calculate = np.isclose(self.relevance_idx[layer_to_ablate][for_neuron], -1) or (return_decreasing
+                            and ((self.most_relevant_concept[layer_to_ablate][for_neuron]['label'] == '') or
+                                 (self.most_relevant_type[layer_to_ablate][for_neuron]['label'] == '')))
+        if need_to_calculate:
             default_path_of_model = os.path.join(network_data.save_path,network_data.model.name+'.h5')
             if for_neuron is None:
-                self.relevance_idx[layer_to_ablate], self.most_relevant_concept[layer_to_ablate], self.most_relevant_type[layer_to_ablate] =\
-                    network_data.get_relevance_by_ablation(layer_analysis=layer_name, neuron=neuron_idx,
+                result = network_data.get_relevance_by_ablation(layer_analysis=layer_name, neuron=neuron_idx,
                                                         layer_to_ablate=layer_to_ablate, path_model=default_path_of_model,
-                                                       return_decreasing=True)
+                                                       return_decreasing=return_decreasing, print_decreasing_matrix=print_decreasing_matrix)
+                if return_decreasing:
+                    self.relevance_idx[layer_to_ablate], self.most_relevant_concept[layer_to_ablate], \
+                    self.most_relevant_type[layer_to_ablate] = result
+                else:
+                    self.relevance_idx[layer_to_ablate] = result
             else:
+                #creation if needed
                 if layer_to_ablate not in self.relevance_idx:
                     self.relevance_idx[layer_to_ablate] = -np.ones(shape=len(network_data.get_layer_by_name(layer_to_ablate).neurons_data), dtype=np.float)
                 if layer_to_ablate not in self.most_relevant_concept:
                     self.most_relevant_concept[layer_to_ablate] = np.zeros(shape=len(network_data.get_layer_by_name(layer_to_ablate).neurons_data), dtype = [('label', 'U64'), ('value', np.float)])
                 if layer_to_ablate not in self.most_relevant_type:
                     self.most_relevant_type[layer_to_ablate] = np.zeros(shape=len(network_data.get_layer_by_name(layer_to_ablate).neurons_data), dtype = [('label', 'U64'), ('value', np.float)])
-                if np.isclose(self.relevance_idx[layer_to_ablate][for_neuron], -1):
-                    self.relevance_idx[layer_to_ablate][for_neuron], self.most_relevant_concept[layer_to_ablate][for_neuron], \
-                    self.most_relevant_type[layer_to_ablate][for_neuron] = \
-                        network_data.get_relevance_by_ablation(layer_analysis=layer_name, neuron=neuron_idx,
+                result = network_data.get_relevance_by_ablation(layer_analysis=layer_name, neuron=neuron_idx,
                                                                layer_to_ablate=layer_to_ablate,
                                                                path_model=default_path_of_model, for_neuron=for_neuron,
-                                                               return_decreasing=True)
+                                                               return_decreasing=return_decreasing,
+                                                           print_decreasing_matrix=print_decreasing_matrix)
+                if return_decreasing:
+                    self.relevance_idx[layer_to_ablate][for_neuron], self.most_relevant_concept[layer_to_ablate][for_neuron], \
+                    self.most_relevant_type[layer_to_ablate][for_neuron] = result
+                else:
+                    self.relevance_idx[layer_to_ablate] = result
+
 
 
             print('Relevance: '+layer_name+' '+str(neuron_idx)+'/'+str(len(network_data.get_layer_by_name(layer_name).neurons_data)))
         if not return_decreasing:
-            return self.relevance_idx[layer_to_ablate]
+            if for_neuron is None:
+                return self.relevance_idx[layer_to_ablate]
+            else:
+                return self.relevance_idx[layer_to_ablate][for_neuron]
         else:
-            return self.relevance_idx[layer_to_ablate][for_neuron], self.most_relevant_concept[layer_to_ablate][for_neuron], self.most_relevant_type[layer_to_ablate][for_neuron]
+            if for_neuron is None:
+                return self.relevance_idx[layer_to_ablate], self.most_relevant_concept[layer_to_ablate], self.most_relevant_type[layer_to_ablate]
+            else:
+                return self.relevance_idx[layer_to_ablate][for_neuron], self.most_relevant_concept[layer_to_ablate][for_neuron], self.most_relevant_type[layer_to_ablate][for_neuron]
 
+    def get_most_relevant_type(self, network_data, layer_name, neuron_idx, layer_to_ablate, for_neuron=None):
+        _,_, most_relevant_type = self.get_relevance_idx(network_data=network_data, layer_name=layer_name, neuron_idx=neuron_idx,
+                                                         layer_to_ablate=layer_to_ablate, for_neuron=for_neuron,
+                                                         return_decreasing=True)
+        return most_relevant_type
+
+    def get_most_relevant_concept(self, network_data, layer_name, neuron_idx, layer_to_ablate, for_neuron=None):
+        _, most_relevant_concept, _ = self.get_relevance_idx(network_data=network_data, layer_name=layer_name,
+                                                          neuron_idx=neuron_idx,
+                                                          layer_to_ablate=layer_to_ablate, for_neuron=for_neuron,
+                                                             return_decreasing=True)
+        return most_relevant_concept
 
     def color_selectivity_idx(self, network_data, layer_name, neuron_idx,  type='mean', th = 0.1,
                               activations_masks=None):