24c05693a8c2320b15d9e8ee7166b436f626ac2b
arias2202 <33318527+arias2202@users.noreply.github.com>
2022-03-01 11:11:08 +0100

    Update image.py

--- functions/image.py
+++ functions/image.py
@@ -179,7 +179,7 @@ class ImageDataset():
 
         :return: PIL image instance.
         """
-        print(img_name)
+        print(crop_pos)
         img = self._load_image(img_name)
 
         # Add padding
